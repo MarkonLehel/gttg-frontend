bbf4cab41a83523b2fd044d4f2f717c1540f4b01
MatthewHungarian <mate.m.magyar@gmail.com>
2021-09-22 19:36:04 +0200

    Change button action from useState to Stransition hook

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from './components/layout/Header';
 import Booker from './components/Booker';
 import About from './components/pages/About';
 import { useState } from 'react';
-import {useTransition} from 'react-spring';
+import {useTransition, animated} from 'react-spring';
 
 const App = props=> {
   const [isVisible, setIsVisible] = useState(false);
   const transition = useTransition(isVisible, {
     
-  })
+  });
   return (
     <>
     <div className="container">
@@ -36,9 +36,9 @@ const App = props=> {
         //
       }}>{isVisible ? 'unmount' : 'mount'}</button>
       <div className="planets-card-container">
-        {isVisible ? <div className="item"></div> : ''}
-        {transition((style, item) => //callback with 2 arg
-            item ? <div className="item"></div> : ''
+        {/* {isVisible ? <div className="item"></div> : ''} */}
+        {transition((style, item) => //callback with 2 arg = check item and animate with style
+            item ? <animated.div style={style} className="item"></animated.div> : ''
         )}
          </div>
       </div>
