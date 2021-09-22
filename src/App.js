d614a62b0a34f247a7a95e4526e54af295517b2d
MatthewHungarian <mate.m.magyar@gmail.com>
2021-09-22 19:31:43 +0200

    Use transition hook with 2 args as callback instead of hide

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Header from './components/layout/Header';
 import Booker from './components/Booker';
 import About from './components/pages/About';
 import { useState } from 'react';
+import {useTransition} from 'react-spring';
 
 const App = props=> {
   const [isVisible, setIsVisible] = useState(false);
+  const transition = useTransition(isVisible, {
+    
+  })
   return (
     <>
     <div className="container">
@@ -30,9 +34,12 @@ const App = props=> {
       <button onClick={() => {
         setIsVisible(v => !v);
         //
-      }}>mount</button>
+      }}>{isVisible ? 'unmount' : 'mount'}</button>
       <div className="planets-card-container">
         {isVisible ? <div className="item"></div> : ''}
+        {transition((style, item) => //callback with 2 arg
+            item ? <div className="item"></div> : ''
+        )}
          </div>
       </div>
     </>
