6ed85ab26c757d7f13fdd0d478a1cba4e866cd1a
MatthewHungarian <mate.m.magyar@gmail.com>
2021-09-22 19:20:01 +0200

    Plug in test button for element action

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import  { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Booker from './components/Booker';
 import About from './components/pages/About';
+import { useState } from 'react';
 
 const App = props=> {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <>
     <div className="container">
@@ -26,10 +28,11 @@ const App = props=> {
     
     <div className="planets-segment">
       <button onClick={() => {
+        setIsVisible(v => !v);
         //
       }}>mount</button>
       <div className="planets-card-container">
-        <div className="item"></div>
+        {isVisible ? <div className="item"></div> : ''}
          </div>
       </div>
     </>
