4f0d4744844d264f34055a6e89329a1f7b8db2cb
agocsvince <64377515+agocsvince@users.noreply.github.com>
2021-09-23 20:18:20 +0200

    Revert "Main page"

--- src/App.js
+++ src/App.js
@@ -4,41 +4,26 @@ import Header from './components/layout/Header';
 import Booker from './components/Booker';
 import About from './components/pages/About';
 import PlanetList from './components/PlanetList';
-import EventList from './components/EventList';
-import { PlanetProvider } from './components/PlanetContext';
-import { EventProvider } from './components/EventContext';
-import TripList from './components/TripList';
-import { TripProvider } from './components/TripContext';
-import Footer from './components/layout/Footer';
 
 const App = props=> {
   return (
     <div className="App">
       <Router>
-        <PlanetProvider>
-          <EventProvider>
-            <TripProvider>
-              <Header />
-              <div className="container">
-                <div id="content">
-                  <Route exact path="/">
-                    <PlanetList />
-                    <Booker />
-                    <EventList />
-                    <TripList />
-                  </Route>
-                  <Route path="/top-five">
-                    
-                  </Route>
-                  <Route path="/about">
-                    <About />
-                  </Route>
-                </div>
-              </div>
-              <Footer/>
-            </TripProvider>
-          </EventProvider>
-        </PlanetProvider>
+        <Header />
+        <div className="container">
+          <div id="content">
+            <Route exact path="/">
+              <PlanetList />
+              <Booker />
+            </Route>
+            <Route path="/top-five">
+              
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </div>
+        </div>
       </Router>
     </div>
   );
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,6 @@ const Header = props => {
     return (
         <div id="header">
             <Link to="/" >Home</Link>
-            <Link to="/top-five" >Top5</Link>
             <Link to="about" >About</Link>
         </div>
     )
