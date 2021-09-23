35ed13019b1123d21eba97b0cb4cc77f2155105c
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2021-09-23 10:02:58 +0200

    add planet skeleton

--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -3,18 +3,19 @@ import axios from 'axios';
 import Planet from './Planet';
 
 const PlanetList = props => {
-    const [planets, setPlanets] = useState({})
+    // const [planets, setPlanets] = useState({})
+    const planets = ["res","res","res","res"];
 
-    useEffect(() => {
-        axios.get('https://localhost:5001/api/Planet')
-            .then(res => setPlanets( res ))
-    }, [])
+    // useEffect(() => {
+    //     axios.get('https://localhost:5001/api/Planet')
+    //         .then(res => setPlanets(  ))
+    // }, [])
     
     return (
-        <div>
-            {planets.map(planet => {
+        <div id="planets">
+            {planets.map(planet => 
                 <Planet />
-            })}
+            )}
         </div>
     )
 }
