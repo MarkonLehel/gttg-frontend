ebab0f1a35ad0d81ac434057160bed3044ad3d3a
MatthewHungarian <mate.m.magyar@gmail.com>
2021-10-22 00:47:59 +0200

    Add data-testid to planet

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Planet = props => {
     return (
-        <div className="planet">
+        <div className="planet" data-testid={props.planet.planetName}>
             <img src={`../../public/images${props.planet.imageName}`} alt={props.planet.planetName} />
             <h2>{props.planet.planetName}</h2>
             <p>{props.planet.planetDescription}</p><span>{props.planet.population} people</span>
