289dfcfdef96fc8baabae0c40e537f0f9d9eedbc
MatthewHungarian <mate.m.magyar@gmail.com>
2021-10-21 23:21:37 +0200

    Add footer test with id querySelector

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('should render header division element', () => {
-  const result = render(<App />);
-  const divisionElement = result.container.querySelector('#header');
-  expect(divisionElement).toBeInTheDocument();
-});
-
 test('should render header division element and check by role', () => {
   render(<App />);
   const divisionElement = screen.getByRole("heading");
+  expect(divisionElement).toBeInTheDocument();
+});
+
+test('should render footer division element', () => {
+  const result = render(<App />);
+  const divisionElement = result.container.querySelector('#footer');
   expect(divisionElement).toBeInTheDocument();
 });
