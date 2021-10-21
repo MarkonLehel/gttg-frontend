12d9e1fb588f9e948cdfaa6196c00176eb3d9bf0
MatthewHungarian <mate.m.magyar@gmail.com>
2021-10-21 23:05:13 +0200

    Add test for header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('should render header division element', () => {
+  const result = render(<App />);
+  const divisionElement = result.container.querySelector('#header');
+  expect(divisionElement).toBeInTheDocument();
+});
+
+
+/*import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
